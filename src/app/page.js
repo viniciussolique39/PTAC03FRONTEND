854b4a8c51639677a845935de92eb68072b9af9b
Vinicius Solique <vinicius.solique@estudante.ifms.edu.br>
2023-06-12 20:02:19 -0400

    Alteração de ID para código e localhost 3000 para 3003

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default async function Home() {
 
-  const req = await fetch("http://localhost:3000/produtos", {
+  const req = await fetch("http://localhost:3003/produtos", {
     cache: "no-cache"
   });
   const produtos = await req.json();
@@ -12,13 +12,13 @@ export default async function Home() {
     <main> <Link href="/cadastro" className='voltar'> CADASTRAR </Link>
 
       {produtos.map(produtos => (
-        <div key={produtos.id}>
+        <div key={produtos.codigo}>
           <p>{produtos.titulo}</p>
           <p>{produtos.data_cadastro}</p>
           <p>{produtos.preco}</p>
           <p>{produtos.descricao}</p>
           <p>{produtos.imagem}</p>
-          <Link href={`/produtos/${produtos.id}`}>ver mais</Link>
+          <Link href={`/produtos/${produtos.codigo}`}>ver mais</Link>
         </div>
       ))}
     </main>
