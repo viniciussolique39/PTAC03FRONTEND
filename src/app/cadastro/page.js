8c29720c425cb49d4d3c25385684051ca63bf358
Vinicius Solique <vinicius.solique@estudante.ifms.edu.br>
2023-06-15 08:40:19 -0400

    Alteração da porta de 3000 para 3003

--- src/app/cadastro/page.js
+++ src/app/cadastro/page.js
@@ -22,7 +22,7 @@ export default function Cadastro() {
             imagem: imagem
         }
         const produtosJson = JSON.stringify(produtos);
-        fetch("http://localhost:3000/produtos", {
+        fetch("http://localhost:3003/produtos", {
             method: "POST",
             headers: { "content-Type": "application/json" },
             body: produtosJson
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Projeto aula',
+  title: 'Cadastro de Produtos',
   description: 'Simulando consumo de api com JsonServer',
 }
 
